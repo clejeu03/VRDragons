bfd1805f4fade35355642baf93f739ec9860b5a6
Mahoimi <makahomi@gmail.com>
2014-03-25 16:13:10 +0100

    New paper plane for Solo mode

---
+++ Assets/Scripts/Animate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		animation.Play ("Take 001");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
